4f1861151249f2a55ecda835d6906e08f00624b6
leodlc <ljde@espe.edu.ec>
2022-08-29 23:44:01 -0500

    ljde2

---
+++ js/nav_quienes_somos.js
@@ -0,0 +1,69 @@
+function phylosophy() {
+    document.getElementsByClassName("phylosophy")[0].style.display = "block";
+    document.getElementsByClassName("academic_domains")[0].style.display = "none";
+    document.getElementsByClassName("authorities")[0].style.display = "none";
+    document.getElementsByClassName("structure")[0].style.display = "none";
+    document.getElementsByClassName("hymn")[0].style.display = "none";
+    document.getElementsByClassName("college_history")[0].style.display = "none";
+    document.getElementsByClassName("campus")[0].style.display = "none";
+}
+
+function academic_domains() {
+    document.getElementsByClassName("phylosophy")[0].style.display = "none";
+    document.getElementsByClassName("academic_domains")[0].style.display = "block";
+    document.getElementsByClassName("authorities")[0].style.display = "none";
+    document.getElementsByClassName("structure")[0].style.display = "none";
+    document.getElementsByClassName("hymn")[0].style.display = "none";
+    document.getElementsByClassName("college_history")[0].style.display = "none";
+    document.getElementsByClassName("campus")[0].style.display = "none";
+}
+
+function authorities() {
+    document.getElementsByClassName("phylosophy")[0].style.display = "none";
+    document.getElementsByClassName("academic_domains")[0].style.display = "none";
+    document.getElementsByClassName("authorities")[0].style.display = "block";
+    document.getElementsByClassName("structure")[0].style.display = "none";
+    document.getElementsByClassName("hymn")[0].style.display = "none";
+    document.getElementsByClassName("college_history")[0].style.display = "none";
+    document.getElementsByClassName("campus")[0].style.display = "none";
+}
+
+function structure() {
+    document.getElementsByClassName("phylosophy")[0].style.display = "none";
+    document.getElementsByClassName("academic_domains")[0].style.display = "none";
+    document.getElementsByClassName("authorities")[0].style.display = "none";
+    document.getElementsByClassName("structure")[0].style.display = "block";
+    document.getElementsByClassName("hymn")[0].style.display = "none";
+    document.getElementsByClassName("college_history")[0].style.display = "none";
+    document.getElementsByClassName("campus")[0].style.display = "none";
+}
+
+function hymn() {
+    document.getElementsByClassName("phylosophy")[0].style.display = "none";
+    document.getElementsByClassName("academic_domains")[0].style.display = "none";
+    document.getElementsByClassName("authorities")[0].style.display = "none";
+    document.getElementsByClassName("structure")[0].style.display = "none";
+    document.getElementsByClassName("hymn")[0].style.display = "block";
+    document.getElementsByClassName("college_history")[0].style.display = "none";
+    document.getElementsByClassName("campus")[0].style.display = "none";
+}
+
+function college_history() {
+    document.getElementsByClassName("phylosophy")[0].style.display = "none";
+    document.getElementsByClassName("academic_domains")[0].style.display = "none";
+    document.getElementsByClassName("authorities")[0].style.display = "none";
+    document.getElementsByClassName("structure")[0].style.display = "none";
+    document.getElementsByClassName("hymn")[0].style.display = "none";
+    document.getElementsByClassName("college_history")[0].style.display = "block";
+    document.getElementsByClassName("campus")[0].style.display = "none";
+}
+
+function campus() {
+    document.getElementsByClassName("phylosophy")[0].style.display = "none";
+    document.getElementsByClassName("academic_domains")[0].style.display = "none";
+    document.getElementsByClassName("authorities")[0].style.display = "none";
+    document.getElementsByClassName("structure")[0].style.display = "none";
+    document.getElementsByClassName("hymn")[0].style.display = "none";
+    document.getElementsByClassName("college_history")[0].style.display = "none";
+    document.getElementsByClassName("campus")[0].style.display = "block";
+}
